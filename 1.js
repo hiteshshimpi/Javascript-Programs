b5de4a5ab5db362aed698dfd2cb3640a54e3e8e3
Hitesh Vinod Shimpi <43511832+hiteshshimpi@users.noreply.github.com>
2025-02-23 23:21:41 +0530

    Basic code file of JS

---
+++ 1.js
@@ -0,0 +1,16 @@
+//console.log("Hello World")
+
+let a = 4
+let c = 2.58
+//console.log(a)
+//console.log(typeof(a))
+let b ="Hitesh"
+//console.log(b)
+//console.log(typeof(b))
+
+let req= true
+//console.log(typeof(req))
+ 
+
+let d = a+c
+console.log(!d == 4)
