7cc086f38a54a1bce0e7cb9689d4d5b8cf6368be
Hitesh Vinod Shimpi <43511832+hiteshshimpi@users.noreply.github.com>
2025-03-03 22:19:15 +0530

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,16 @@
+// from 1 to 10 get me multiples of 2 and 5
+
+for(let i=1;i<=10;i++)
+    {
+        if(i % 2 ==0 && i % 5 ==0)
+            {
+                console.log("It is a Multiple of 2 and 5 :" +i)
+            }
+    
+        else
+        {
+            console.log("It is not multiple of 2 and 5 : "+i)
+        }
+        
+    }
+    
